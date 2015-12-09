f068e99e3e2d7a45b6210bff47abc21cb7f1733a
Bugs Bunny <jiri.zajic@mensa.cz>
2015-12-09 16:36:16 +0100

    game state

--- examples/cervici/index.js
+++ examples/cervici/index.js
@@ -7,17 +7,45 @@ server.listen(port, function () {
 
 var io = require('../..')(server);
 
-var counterId = 1; // id
+var counterId = 1;
+var width = 600;
+var height = 300;
 
-var appState = {
-    users: []
-};
+var Consts = {};
 
-var COLORS = [
+Consts.COLORS = [
     '#e21400', '#91580f', '#f8a700', '#f78b00',
     '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
     '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
 ];
+
+Consts.USER_STATE = {
+    IDLE: 'IDLE',
+    READY: 'READY',
+    PLAYING: 'PLAYING'
+};
+
+Consts.GAME_STATE = {
+    IDLE: 'IDLE',
+    COUNTDOWN: 'COUNTDOWN',
+    PLAYING: 'PLAYING'
+};
+
+var appState = {
+    users: [],
+    game: {
+        width: width,
+        height: height,
+        state: Consts.GAME_STATE.IDLE,
+        board: (function() {
+            var arr = [];
+            for (var i = 0; i < width * height; i++) {
+                arr[i] = null;
+            }
+            return arr;
+        }())
+    }
+};
 
 io.on('connection', function(socket) {
     // console.log('someone connected!!!');
@@ -39,7 +67,8 @@ io.on('connection', function(socket) {
         appState.users.push({
             id: socket.userId,
             username: username,
-            color: COLORS[appState.users.length]
+            color: Consts.COLORS[appState.users.length],
+            state: Consts.USER_STATE.IDLE
         });
 
         console.log('number of users: ' + appState.users.length);
@@ -54,6 +83,27 @@ io.on('connection', function(socket) {
         io.emit('USER_JOINED', {
             users: appState.users
         });
+    });
+
+    socket.on('USER_STATE', function(newState) {
+        var user = getUserById(socket.userId);
+
+        if (user.state === newState) return;
+
+        console.log(user.username + ' changing state to ' + newState + '!');
+
+        updateUserById(socket.userId, {
+            state: Consts.USER_STATE.READY
+        });
+
+        io.emit('USER_STATE_CHANGED', {
+            users: appState.users
+        });
+
+        if (isUserGlobalStateReady()) {
+            console.log('everybody ready!')
+            io.emit('GAME_STATE_CHANGED', appState.game);
+        }
     });
 
     // when the user disconnects.. perform this
@@ -86,6 +136,24 @@ io.on('connection', function(socket) {
 function getUserById(id) {
     return appState.users.find(function(user) {
         return user.id === id;
+    });
+}
+
+function updateUserById(id, obj) {
+    appState.users = appState.users.map(function(user) {
+        if (user.id === id) {
+            for (key in obj) {
+                user[key] = obj[key];
+            }
+            return user;
+        }
+        return user;
+    });
+}
+
+function isUserGlobalStateReady() {
+    return appState.users.every(function(user) {
+        return user.state === Consts.USER_STATE.READY;
     });
 }
 
