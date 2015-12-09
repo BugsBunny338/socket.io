ed39de63ba4d19054bb666e91f4f1a9c23a1e931
Bugs Bunny <jiri.zajic@mensa.cz>
2015-12-09 13:18:34 +0100

    login, logout

--- examples/cervici/index.js
+++ examples/cervici/index.js
@@ -1,75 +1,75 @@
-// Setup basic express server
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('../..')(server);
+var server = require('http').createServer();
 var port = process.env.PORT || 3003;
 
 server.listen(port, function () {
-  console.log('Server listening at port %d', port);
+    console.log('Server listening at port %d', port);
 });
 
-// Routing
-app.use(express.static(__dirname + '/public'));
+var io = require('../..')(server);
 
-// Chatroom
+var appState = {
+    users: [] // {id, nick, color}
+};
 
-var numUsers = 0;
+var COLORS = [
+    '#e21400', '#91580f', '#f8a700', '#f78b00',
+    '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
+    '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
+];
 
 io.on('connection', function (socket) {
-  var addedUser = false;
+    console.log('someone connected!!!');
+    // socket.emit('welcome message', { foo: 'bar' });
+    // socket.broadcast.emit('welcome message (broadcast)', { foo: 'bar' });
 
-  // when the client emits 'new message', this listens and executes
-  socket.on('new message', function (data) {
-    // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
+    var addedUser = false;
+
+    socket.on('login', function (username) {
+        if (addedUser) return;
+
+        console.log(username + ' logged in!');
+
+        // we store the id in the socket session for this client
+        socket.id = Date.now();
+
+        addedUser = true;
+
+        appState.users.push({
+            id: socket.id,
+            username: username,
+            color: COLORS[appState.users.length - 1]
+        })
+
+        // echo total number of users to the client
+        socket.emit('USER_LOGGED', {
+            id: appState.users[appState.users.length - 1].id,
+            users: appState.users
+        });
+
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('USER_JOINED', {
+            id: appState.users[appState.users.length - 1].id,
+            users: appState.users
+        });
     });
-  });
 
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', function (username) {
-    if (addedUser) return;
+    // when the user disconnects.. perform this
+    socket.on('disconnect', function () {
+        if (addedUser) {
 
-    // we store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
+            console.log(appState.users.length);
+            
+            appState.users = appState.users.filter(function(user) {
+                return user.id !== socket.id
+            });
+
+            console.log(appState.users.length);
+
+            // echo globally that this client has left
+            socket.broadcast.emit('USER_LEFT', {
+                id: socket.id,
+                users: appState.users
+            });
+        }
     });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
-  });
-
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', function () {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', function () {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
-    });
-  });
-
-  // when the user disconnects.. perform this
-  socket.on('disconnect', function () {
-    if (addedUser) {
-      --numUsers;
-
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    }
-  });
 });
